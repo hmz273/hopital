9ee642af4e7fcb6d5c7a3d57990a674b03e7a15c
hmz273 <73111873+hmz273@users.noreply.github.com>
2021-07-22 13:42:20 +0100

    code

---
+++ Hopital/app/Http/Controllers/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    public function index(){
+        $role=Auth::user()->role;
+
+        if($role=='0'){
+            return view('patient.dbd');
+        }
+    }
+}
---
+++ Hopital/resources/views/welcome.blade.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>HMS</title>
+    <link rel="stylesheet" href="{{asset('assets/bootstrap/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/fonts/font-awesome.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/fonts/ionicons.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/Side-By-Side-Section.css')}}">
+    {{-- <link rel="stylesheet" href="{{asset('assets/css/Animation-Cards-1.css')}}"> --}}
+    <link rel="stylesheet" href="{{asset('assets/css/Animation-Cards.css')}}">
+    {{-- <link rel="stylesheet" href="{{asset('assets/css/Dark-NavBar-1.css')}}"> --}}
+    {{-- <link rel="stylesheet" href="{{asset('assets/css/Dark-NavBar-2.css')}}"> --}}
+    <link rel="stylesheet" href="{{asset('assets/css/Dark-NavBar.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/Footer-Dark.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/Footer-with-social-media-icons.css')}}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.1.1/aos.css">
+    {{-- <link rel="stylesheet" href="{{asset('assets/css/Slider-laptop-1.css')}}"> --}}
+    <link rel="stylesheet" href="{{asset('assets/css/Slider-laptop.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/styles.css')}}">
+</head>
+
+<body>
+    <div>
+        <nav class="navbar navbar-light navbar-expand-md sticky-top navigation-clean-button" style="height:80px;background-color:#18454D;color:#ffffff;">
+            <div class="container-fluid"><a class="navbar-brand" href="/">HMS</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+                <div
+                    class="collapse navbar-collapse" id="navcol-1">
+                    <ul class="nav navbar-nav ml-auto">
+                        <li class="nav-item" role="presentation"><a class="nav-link active" style="color:#ffffff;" href="/">Dashboard</a></li>
+                        <li class="nav-item" role="presentation"><a class="nav-link active" style="color:#ffffff;" href="/contact">About Us</a></li>
+                        <li class="nav-item" role="presentation"><a class="nav-link active" style="color:#ffffff;" href="#">Contact Us</a></li>
+                        <li class="nav-item" role="presentation"><a class="nav-link active" style="color:#ffffff;" href="#">Sign In</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+    <div class="x_001_laptop_main_wrap">
+        <div class="row">
+            <div class="col-md-7">
+                <div class="x_001_laptop_inner_wrap">
+                    <img src="assets/img/undraw_doctors_hwty.svg" alt="EROR_101">
+                </div>
+            </div>
+            <div class="col-md-5">
+                <h1>Hospital Management System</h1>
+                <p>
+                    is an integrated software that handles different directions of clinic workflows. 
+                    It manages the smooth healthcare performance along with administrative, medical, 
+                    legal and financial control.
+                </p>
+                <a href="/about">read more</a>
+            </div>
+        </div>
+    </div>
+    <div class="row space-rows">
+        {{-- CARD 1 --}}
+        <div class="col"> 
+            <div class="card cards-shadown cards-hover" data-aos="flip-left" data-aos-duration="950">
+                <div class="card-header flx">
+                    <img src='assets/img/1.png' style=" width: 55%;">
+                </div>
+                <div class="card-body">
+                    <h4 class="card-text sub-text-color">QQQQQQQQQQ</h4>
+                    <p class="card-text cardbody-sub-text">Faux text Faux text Faux text Faux text Faux text Faux text Faux text </p>
+                </div>
+            </div>
+        </div>
+        
+        {{-- CARD 2 --}}
+        <div class="col">
+            <div class="card cards-shadown cards-hover" data-aos="slide-right" data-aos-duration="950">
+                <div class="card-header flx">
+                    <img src='assets/img/2.png' style=" width: 55%;">
+                </div>
+                <div class="card-body">
+                    <h4 class="card-text sub-text-color">QQQQQQQQQQ</h4>
+                    <p class="card-text cardbody-sub-text">Faux text Faux text Faux text Faux text Faux text Faux text Faux text </p>
+                </div>
+            </div>
+        </div>
+
+        {{-- CARD 3 --}}
+        <div class="col">
+            <div class="card cards-shadown cards-hover" data-aos="flip-up" data-aos-duration="950">
+                <div class="card-header cards-header-hover flx">
+                    <img src='assets/img/3.png' style=" width: 45%;">
+                </div>
+                <div class="card-body">
+                    <h4 class="card-text sub-text-color">QQQQQQQQQQ</h4>
+                    <p class="card-text cardbody-sub-text">Faux text Faux text Faux text Faux text Faux text Faux text Faux text </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <section class="index-howto-alt py-3">
+        <div class="container">
+            {{-- <div class="psd-line pos-rel" style="position:relative;"> --}}
+                <div class="row flex-column-reverse align-items-center flex-sm-row">
+                    <div class="col-12 col-sm-6 d-flex flex-column justify-content-center p-5 psd-right1">
+                        <p class="m-0 text-center text-sm-right text-light">Faux Text Faux Text Faux Text Faux 
+                            Text Faux Text Faux Text Faux Text Faux Text Faux Text Faux Text Faux Text Faux 
+                            Text Faux Text Faux Text Faux Text Faux Text Faux Text Faux Text Faux Text Faux 
+                            Text Faux Text Faux Text Faux Text Faux Text.&nbsp;<br></p>
+                    </div>
+                    <div class="col-12 col-sm-6 d-flex flex-row justify-content-center align-items-center intro-card-2 p-5">
+                        <img class="howto-img" src="assets/img/lady-doctor.png">
+                    </div>
+                </div>
+                <div class="row align-items-center">
+                    <div class="col-12 col-sm-6 d-flex flex-row justify-content-center align-items-center intro-card-3 p-5">
+                        <img class="howto-img" src="assets/img/doctor.png">
+                    </div>
+                    <div class="col-12 col-sm-6 d-flex flex-column justify-content-center p-5 psd-left2">
+                        <p class="m-0 text-center text-sm-left text-light">Faux Text Faux Text Faux Text Faux 
+                            Text Faux Text Faux Text Faux Text Faux Text Faux Text Faux Text Faux Text Faux 
+                            Text Faux Text Faux Text Faux Text Faux Text Faux Text Faux Text Faux Text Faux 
+                            Text Faux Text Faux Text Faux Text Faux Text.<br></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="footer-dark">
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6 col-md-3 item">
+                        <h3>Services</h3>
+                        <ul>
+                            <li><a href="#">Web design</a></li>
+                            <li><a href="#">Development</a></li>
+                            <li><a href="#">Hosting</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-6 col-md-3 item">
+                        <h3>About</h3>
+                        <ul>
+                            <li><a href="#">Company</a></li>
+                            <li><a href="#">Team</a></li>
+                            <li><a href="#">Careers</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-6 item text">
+                        <h3>Company Name</h3>
+                        <p>Praesent sed lobortis mi. Suspendisse vel placerat ligula. Vivamus ac sem lacus. Ut vehicula rhoncus elementum. Etiam quis tristique lectus. Aliquam in arcu eget velit pulvinar dictum vel in justo.</p>
+                    </div>
+                    <div class="col item social"><a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-instagram"></i></a></div>
+                </div>
+                <p class="copyright">Company Name © 2017</p>
+            </div>
+        </footer>
+    </div>
+    <script src="{{asset('assets/js/jquery.min.js')}}"></script>
+    <script src="{{asset('assets/bootstrap/js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('assets/js/bs-init.js')}}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.1.1/aos.js"></script>
+    <script src="{{asset('assets/js/Slider-laptop.js')}}"></script>
+</body>
+
+</html>
